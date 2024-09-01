6c0e6591d6cdd75bf14e0fa752072644f76e6aa5
Esmael Aliyi Uta <esmaelaliyi9@gmail.com>
2024-09-02 02:21:24 +0800

    python script

--- python.py
+++ python.py
@@ -1,11 +1,4 @@
-print("Hello")
-#Asking the user for input
-inp= input("Tell me your name: \n")
-print("Your name is: "+ inp)
-
-prompt = 'What the speed of light?\n'
-speed_of_light = int(input(prompt))
-time = 'what time does it take to reach the earth?\n'
-time_taken= int(input(time))
-distance = int(speed_of_light * time_taken)
-print(str(distance) + 'Km')
+import random
+for i in range(1, 20):
+    x = random.random()
+    print(x)
